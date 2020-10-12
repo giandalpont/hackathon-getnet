a1068b70dd2c944b3aea0e8a2f751a3f59a1a668
Jean Junior <jclgjunior@gmail.com>
2020-10-12 16:36:31 -0300

    Overview

--- web/src/Components/Overview/index.tsx
+++ web/src/Components/Overview/index.tsx
@@ -1,34 +1,89 @@
 import React from 'react';
 
-import { Container, Cards, Chart } from './styles';
+import { Container, Cards, Chart, TopMenu } from './styles';
 
 const Overview: React.FC = () => {
   return (
 		<Container>
+
+			<TopMenu>
+				<div className="title">Visão Geral</div>
+
+				<span className="items">
+					<img src="" alt="search"/>
+					<img src="" alt="push"/>
+
+					<span className="user">
+						<span className="username">Marianda Silva</span>
+						<img src="" alt="user profile-pic"/>
+					</span>
+				</span>
+			</TopMenu>
+
 			<Cards>
 				<div className="card">
-					<h3>Vendas</h3>
-					<h1>328</h1>
+					<span>
+						<h3>Vendas</h3>
+						<h1>328</h1>
+					</span>
 				</div>
 
 				<div className="card">
-					<h3>Compras</h3>
-					<h1>16</h1>
+					<span>
+						<h3>Compras</h3>
+						<h1>16</h1>
+					</span>
 				</div>
 
 				<div className="card">
-					<h3>Faturamento do mês</h3>
-					<h1>R$ 16.100</h1>
+					<span>
+						<h3>Faturamento do mês</h3>
+						<h1>R$ 16.100</h1>
+					</span>
 				</div>
 
 				<div className="card">
-					<h3>Antecipar</h3>
-					<h1>R$ 1.000</h1>
+					<span>
+						<h3>Antecipar</h3>
+						<h1>R$ 1.000</h1>
+					</span>
 				</div>
 			</Cards>
 
 			<Chart>
-				asd
+				<div className="chart">
+					<h2>Panorama Anual</h2>
+					<span>12 Outubro 2020, 09:41 AM</span>
+
+
+				</div>
+
+				<div className="data">
+					<div className="item">
+						<h3>Crédito</h3>
+						<h1>R$ 449,98</h1>
+					</div>
+
+					<div className="item">
+						<h3>Débito</h3>
+						<h1>R$ 426,99</h1>
+					</div>
+
+					<div className="item">
+						<h3>GetPay</h3>
+						<h1>R$ 3390,00</h1>
+					</div>
+
+					<div className="item">
+						<h3>Pontos</h3>
+						<h1>94</h1>
+					</div>
+
+					<div className="item">
+						<h3>Referência</h3>
+						<h1>31%</h1>
+					</div>
+				</div>
 			</Chart>
 			
 		</Container>
--- web/src/Components/Overview/styles.ts
+++ web/src/Components/Overview/styles.ts
@@ -1,6 +1,31 @@
 import styled from 'styled-components';
 
+export const TopMenu = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  height: 3rem;
+  margin-bottom: 2rem;
+  /* outline: 1px solid; */
+  
+
+  color: #252733;
+`;
+
 export const Container = styled.div`
+  min-width: calc(100vw - 255px);
+  max-height: 100vh;
+  padding: 1rem 2rem;
+  overflow-y: hidden;
+
+  h1 {
+      color: #252733;
+    }
+
+  h3 {
+    color: #9FA2B4;
+  }
 
   .cards {
     display: flex;
@@ -16,6 +41,15 @@ export const Cards = styled.div`
 
   font-weight: 700;
 
+  h1 {
+    font-size: 40px;
+    margin: .3rem;
+  }
+
+  h3 {
+    font-size: 19px;
+  }
+
   .card {
     flex: 1;
 
@@ -23,25 +57,52 @@ export const Cards = styled.div`
     border: 1px solid #DFE0EB;
     border-radius: 10px;
 
-    padding: .5rem;
+    padding: 1rem;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
 
-
-    h1 {
-      color: #252733;
-    }
-
-    h3 {
-      color: #9FA2B4;
-    }
+    text-align: center;
+    
   }
 `;
 
 export const Chart = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 1rem;
+
+  border: 1px solid #DFE0EB;
+  border-radius: 10px;
   background-color: #fff;
-  width: 100%;
-  height: 100%;
+  color: #252733;
+
+  span{
+    color: #9FA2B4;
+  }
+
+  .chart {
+    flex: 2.5;
+    padding: 1rem;
+  }
+
+  .data {
+    flex: 1;
+
+    h1 {
+      font-size: 24px;
+    }
+
+    h3 {
+      font-size: 18px;
+    }
+
+    .item {
+      border-left: 1px solid #DFE0EB;
+      border-bottom: 1px solid #DFE0EB;
+      /* padding: 1rem; */
+      text-align: center
+    }
+  }
 `;
